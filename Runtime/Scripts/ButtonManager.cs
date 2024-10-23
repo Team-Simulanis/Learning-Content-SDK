1561bb4ca1263523785ba902d382668d73cb090b
Simulanis-RohitSharma <rohitsharma@simulanis.com>
2024-10-23 11:13:16 +0530

    ButtonManager Added

---
+++ Runtime/Scripts/ButtonManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simulanis.ContentSDK
+{
+    public class ButtonManager : MonoBehaviour
+    {
+        public void MenuButtonClicked()
+        {
+            
+        }
+        public void HomeButtonClicked()
+        {
+
+        }
+        public void BackButtonClicked()
+        {
+
+        }
+        public void QuitButtonClicked()
+        {
+
+        }
+    }
+}
