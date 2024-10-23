9fb3a21ad7633e1ccbcd9051215be26fafa2a9d0
Simulanis-RohitSharma <rohitsharma@simulanis.com>
2024-10-23 13:48:13 +0530

    Added Language

---
+++ Runtime/Scripts/LanguageSelectionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simulanis.ContentSDK
+{
+    public class LanguageSelectionManager : MonoBehaviour
+    {
+        private string _currentLanguage;
+
+        // Property to get and set the current language
+        public string CurrentLanguage
+        {
+            get { return _currentLanguage; }
+            set 
+            { 
+                _currentLanguage = value;//"English", "Hindi"   -  English is Default
+            }
+        }
+    }
+}
